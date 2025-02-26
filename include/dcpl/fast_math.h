9b30d47816ff69ae2999d95c76c618d3b0290d53
Davide Libenzi <davide.libenzi@gmail.com>
2025-02-26 22:36:18 +0100

    Minor refactoring.

--- include/dcpl/fast_math.h
+++ include/dcpl/fast_math.h
@@ -4,21 +4,23 @@
 #include <cstdint>
 
 namespace dcpl::fmath {
+namespace {
+
+union dpack {
+  double d;
+  std::int64_t i;
+};
+
+}
 
 static inline double log(double value) {
-  union {
-    double d;
-    std::int64_t i;
-  } ux = { value };
+  dpack ux = { value };
 
   return (ux.i - 4606921278410026770LL) * 1.539095918623324e-16;
 }
 
 static inline double exp(double value) {
-  union {
-    double d;
-    std::int64_t i;
-  } ux, vx;
+  dpack ux, vx;
 
   ux.i = static_cast<std::int64_t>(3248660424278399LL * value + 0x3fdf127e83d16f12LL);
   vx.i = static_cast<std::int64_t>(0x3fdf127e83d16f12LL - 3248660424278399LL * value);
