2bd789238ad1a1ea30a1fc7d6efd88bdb0ad06a4
Davide Libenzi <davide.libenzi@gmail.com>
2025-02-26 22:33:15 +0100

    Added fast exp().

--- include/dcpl/fast_math.h
+++ include/dcpl/fast_math.h
@@ -14,5 +14,17 @@ static inline double log(double value) {
   return (ux.i - 4606921278410026770LL) * 1.539095918623324e-16;
 }
 
+static inline double exp(double value) {
+  union {
+    double d;
+    std::int64_t i;
+  } ux, vx;
+
+  ux.i = static_cast<std::int64_t>(3248660424278399LL * value + 0x3fdf127e83d16f12LL);
+  vx.i = static_cast<std::int64_t>(0x3fdf127e83d16f12LL - 3248660424278399LL * value);
+
+  return ux.d / vx.d;
+}
+
 }
 
