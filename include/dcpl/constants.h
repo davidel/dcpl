45ed2950aae6459722d26cf3db061ce07744743e
Davide Libenzi <davide.libenzi@gmail.com>
2025-03-01 14:19:17 +0100

    Minor refactoring.

--- include/dcpl/constants.h
+++ include/dcpl/constants.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <cstdint>
+#include <cstddef>
 
 namespace dcpl {
 namespace consts {
 
 static constexpr std::size_t all = static_cast<std::size_t>(-1);
 static constexpr std::size_t invalid_index = static_cast<std::size_t>(-1);
+static constexpr std::size_t invalid_size = static_cast<std::size_t>(-1);
 
 }
 
