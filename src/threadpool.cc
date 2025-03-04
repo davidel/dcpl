13cbe70ea81067fee786d0a1044681e803dc2e10
Davide Libenzi <davide.libenzi@gmail.com>
2025-03-04 17:08:39 +0100

    Added TP env config.

--- src/threadpool.cc
+++ src/threadpool.cc
@@ -1,9 +1,16 @@
 #include "dcpl/threadpool.h"
 
+#include "dcpl/env.h"
+
 namespace dcpl {
+namespace {
+
+static const std::size_t num_threads = getenv<std::size_t>("DCPL_NUM_THREADS", 0);
+
+}
 
 threadpool* threadpool::create_system_pool() {
-  return new threadpool();
+  return new threadpool(num_threads);
 }
 
 threadpool* threadpool::get() {
