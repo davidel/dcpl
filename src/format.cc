b84d73a0a874ea9575b53d00ad5dbc4fbc56d46a
Davide Libenzi <davide.libenzi@gmail.com>
2025-03-06 19:58:49 +0100

    Use microseconds for duration format.

--- src/format.cc
+++ src/format.cc
@@ -11,11 +11,11 @@ namespace dcpl {
 std::string format_duration(double elapsed) {
   DCPL_CHECK_GE(elapsed, 0.0) << "Duration must be non negative";
 
-  std::intmax_t msec_time = static_cast<std::intmax_t>(elapsed * 1000.0);
+  std::intmax_t usec_time = static_cast<std::intmax_t>(elapsed * 1e6);
   std::imaxdiv_t dr;
 
-  dr = std::imaxdiv(msec_time, 1000);
-  unsigned int msecs = static_cast<unsigned int>(dr.rem);
+  dr = std::imaxdiv(usec_time, 1000000);
+  unsigned int usecs = static_cast<unsigned int>(dr.rem);
 
   dr = std::imaxdiv(dr.quot, 60);
   unsigned int secs = static_cast<unsigned int>(dr.rem);
@@ -38,7 +38,7 @@ std::string format_duration(double elapsed) {
   if (mins > 0) {
     ss << format("%02um:", mins);
   }
-  ss << format("%02us:", secs) << msecs;
+  ss << format("%02us:", secs) << format("%06ju", usecs);
 
   return ss.str();
 }
