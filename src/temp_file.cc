9d3094d3c5bea97e81b339b4c398117933b205e5
Davide Libenzi <davide.libenzi@gmail.com>
2025-03-11 20:04:11 +0100

    Add IN mode.

--- src/temp_file.cc
+++ src/temp_file.cc
@@ -7,7 +7,7 @@ namespace dcpl {
 
 temp_file::temp_file(std::ios::openmode mode, std::optional<std::string> path) :
     path_(path.has_value() ? temp_path(*path) : temp_path()),
-    file_(open(path_, mode | std::ios::out | std::ios::trunc)) {
+    file_(open(path_, mode | std::ios::in | std::ios::out | std::ios::trunc)) {
 }
 
 temp_file::~temp_file() {
