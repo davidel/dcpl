382ea86e77d6768fabe43fb18c1dbff4d0488927
Davide Libenzi <davide.libenzi@gmail.com>
2025-03-19 14:35:07 +0100

    Added some hash bits.

--- include/dcpl/constants.h
+++ include/dcpl/constants.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <climits>
 #include <cstddef>
+#include <initializer_list>
 
 namespace dcpl {
 namespace consts {
@@ -8,6 +10,24 @@ namespace consts {
 static constexpr std::size_t all = static_cast<std::size_t>(-1);
 static constexpr std::size_t invalid_index = static_cast<std::size_t>(-1);
 static constexpr std::size_t invalid_size = static_cast<std::size_t>(-1);
+
+template <typename T, typename S>
+constexpr T load(const std::initializer_list<S> values) {
+  constexpr std::size_t tbits = sizeof(T) * CHAR_BIT;
+  constexpr std::size_t sbits = sizeof(S) * CHAR_BIT;
+  T value = 0;
+  std::size_t nbits = 0;
+
+  for (const auto& svalue : values) {
+    if (nbits >= tbits) {
+      break;
+    }
+    value = (value << sbits) | svalue;
+    nbits += sbits;
+  }
+
+  return value;
+}
 
 }
 
--- include/dcpl/hash.h
+++ include/dcpl/hash.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <functional>
+
+#include "dcpl/constants.h"
+#include "dcpl/core_utils.h"
+#include "dcpl/types.h"
 
 namespace dcpl {
 
@@ -10,6 +15,50 @@ std::size_t hash_combine(std::size_t seed, const T& v) {
   std::hash<T> hasher;
 
   return seed ^ (hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2));
+}
+
+template <typename T = std::size_t>
+T hash_bytes(const void* ptr, std::size_t len, T seed) {
+  constexpr std::size_t shift_bits = (3 * bit_sizeof<T>()) / 4 - 1;
+  constexpr T mul = consts::load<T, std::uint32_t>({
+      0xc6a4a793UL,
+      0x5bd1e995UL,
+      0xab0e9789UL,
+      0x38b34ae5UL
+    });
+
+  const char* buf = static_cast<const char*>(ptr);
+  const char* end = buf + len / sizeof(T);
+  T hash = seed ^ (len * mul);
+
+  for (const char* p = buf; p != end; p += sizeof(T)) {
+    T data = vload<T>(p);
+
+    data *= mul;
+    data ^= data >> shift_bits;
+    data *= mul;
+    hash ^= data;
+    hash *= mul;
+  }
+
+  dcpl::ssize_t left = len % sizeof(T);
+
+  if (left != 0) {
+    T data = 0;
+
+    for (--left; left >= 0; --left) {
+      data = (data << 8) | end[left];
+    }
+
+    hash ^= data;
+    hash *= mul;
+  }
+
+  hash ^= hash >> shift_bits;
+  hash *= mul;
+  hash ^= hash >> shift_bits;
+
+  return hash;
 }
 
 }
