c07b1f371206c796c9f5805a1eb44549f3078ecd
Davide Libenzi <davide.libenzi@gmail.com>
2025-03-27 14:53:44 +0100

    Added one_of template.

--- include/dcpl/type_traits.h
+++ include/dcpl/type_traits.h
@@ -7,4 +7,11 @@ namespace dcpl {
 template <typename T>
 using remove_cvr = typename std::remove_reference_t<typename std::remove_cv_t<T>>;
 
+template <typename T, typename... Ts>
+struct one_of : std::disjunction<std::is_same<T, Ts>...> { };
+
+template <typename T, typename... Ts>
+constexpr bool one_of_v = one_of<T, Ts...>::value;
+
 }
+
