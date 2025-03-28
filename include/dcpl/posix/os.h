dc6dbfc7e2ec6a3a775a67a1d3d34a9a3fa7af1f
Davide Libenzi <davide.libenzi@gmail.com>
2025-03-28 14:30:20 +0100

    Added portable threadsafe localtime.

--- include/dcpl/posix/os.h
+++ include/dcpl/posix/os.h
@@ -2,12 +2,16 @@
 
 #include <unistd.h>
 
+#include <ctime>
+
 namespace dcpl {
 namespace os {
 
 using pid_t = ::pid_t;
 
 pid_t getpid();
+
+std::tm localtime(std::time_t epoc_time);
 
 }
 
--- src/posix/os.cc
+++ src/posix/os.cc
@@ -1,10 +1,20 @@
 #include "dcpl/os.h"
 
+#include <math.h>
+
 namespace dcpl {
 namespace os {
 
 pid_t getpid() {
   return ::getpid();
+}
+
+std::tm localtime(std::time_t epoc_time) {
+  std::tm time_data;
+
+  ::localtime_r(&epoc_time, &time_data);
+
+  return time_data;
 }
 
 }
