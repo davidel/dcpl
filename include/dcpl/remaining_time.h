065071333defccc8c17adce1ec1018f0a67f5c72
Davide Libenzi <davide.libenzi@gmail.com>
2025-04-09 09:43:27 +0200

    Added time to estimate remaining time.

---
+++ include/dcpl/remaining_time.h
@@ -0,0 +1,31 @@
+#include "dcpl/types.h"
+#include "dcpl/utils.h"
+
+namespace dcpl {
+
+template <typename T>
+class remaining_time {
+ public:
+  explicit remaining_time(T count) :
+      start_(nstime()),
+      count_(count) {
+  }
+
+  ns_time estimate(T current_count) const {
+    if (current_count >= count_) [[unlikely]] {
+      return 0;
+    } else {
+      ns_time now = nstime();
+      ns_time step = (now - start_) / current_count;
+
+      return step * (count_ - current_count);
+    }
+  }
+
+ private:
+  ns_time start_;
+  T count_ = 0;
+};
+
+}
+
