9d46604e465383367b441b8e11cfb51501af153b
Davide Libenzi <davide.libenzi@gmail.com>
2025-04-21 18:07:27 +0200

    Minor optimization.

--- include/dcpl/sequence.h
+++ include/dcpl/sequence.h
@@ -1,21 +1,24 @@
 #include <algorithm>
 #include <cstddef>
+#include <memory>
 #include <numeric>
-#include <vector>
 
 namespace dcpl {
 
 template <typename T, typename S>
 std::size_t levenshtein(const T& s1, const S& s2) {
-  std::vector<std::size_t> current(s2.size() + 1);
-  std::vector<std::size_t> prev(s2.size() + 1);
+  std::size_t size = s2.size();
+  std::unique_ptr<std::size_t[]> storage =
+      std::make_unique<std::size_t[]>(2 * (size + 1));
+  std::size_t* prev = storage.get();
+  std::size_t* current = prev + (size + 1);
 
-  std::iota(prev.begin(), prev.end(), 0);
+  std::iota(prev, current, 0);
 
   for (std::size_t i = 1; i <= s1.size(); ++i) {
     current[0] = i;
 
-    for (std::size_t j = 1; j <= s2.size(); ++j) {
+    for (std::size_t j = 1; j <= size; ++j) {
       std::size_t cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
 
       current[j] = std::min({ prev[j] + 1,
@@ -25,7 +28,7 @@ std::size_t levenshtein(const T& s1, const S& s2) {
     std::swap(prev, current);
   }
 
-  return prev[s2.size()];
+  return prev[size];
 }
 
 }
