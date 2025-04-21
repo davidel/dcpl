127b117d78ec7c61a74e94d0d80587ca554ae3a5
Davide Libenzi <davide.libenzi@gmail.com>
2025-04-21 18:09:17 +0200

    Minor refactoring.

--- include/dcpl/sequence.h
+++ include/dcpl/sequence.h
@@ -7,18 +7,18 @@ namespace dcpl {
 
 template <typename T, typename S>
 std::size_t levenshtein(const T& s1, const S& s2) {
-  std::size_t size = s2.size();
+  std::size_t size2 = s2.size();
   std::unique_ptr<std::size_t[]> storage =
-      std::make_unique<std::size_t[]>(2 * (size + 1));
+      std::make_unique<std::size_t[]>(2 * (size2 + 1));
   std::size_t* prev = storage.get();
-  std::size_t* current = prev + (size + 1);
+  std::size_t* current = prev + (size2 + 1);
 
   std::iota(prev, current, 0);
 
   for (std::size_t i = 1; i <= s1.size(); ++i) {
     current[0] = i;
 
-    for (std::size_t j = 1; j <= size; ++j) {
+    for (std::size_t j = 1; j <= size2; ++j) {
       std::size_t cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
 
       current[j] = std::min({ prev[j] + 1,
@@ -28,7 +28,7 @@ std::size_t levenshtein(const T& s1, const S& s2) {
     std::swap(prev, current);
   }
 
-  return prev[size];
+  return prev[size2];
 }
 
 }
