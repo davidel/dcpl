d6028a8c589bd3f0bd72b84c771646b2dc521f0c
Davide Libenzi <davide.libenzi@gmail.com>
2025-11-18 11:56:42 +0100

    Added a bit-setter utility for bit clear+set APIs.

--- include/dcpl/bit_field.h
+++ include/dcpl/bit_field.h
@@ -23,5 +23,23 @@ struct bit_field {
   unsigned int count = 0;
 };
 
+template <typename T = std::size_t>
+struct bit_setter {
+  using value_type = T;
+
+  explicit bit_setter(T init = 0) : value(init) { }
+
+  template <typename U>
+  T set(const bit_field<U>& field, T fvalue) {
+    value = field.set(value, fvalue);
+    mask |= field.mask();
+
+    return value;
+  }
+
+  T value = 0;
+  T mask = 0;
+};
+
 }
 
